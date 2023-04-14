74aac8c1b608b0592ac474a50b08d3d889591b99
shahidmahmood13 <shahid.mehmood@xloopdigital.com>
2023-04-14 10:46:25 +0500

    task done

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Goal ({text}){
     return (
         <div className="goal">
-            <p>{text}</p>
+            <p className="goallist">{text}</p>
         </div>
     )
 }
